08ddbeafa1fa29e2ec14ec183c0433da622bd944
EC2 Default User <ec2-user@ip-172-31-46-164.eu-west-1.compute.internal>
2018-05-21 09:22:07 +0000

    Creation of the files

---
+++ js/CourseModule4.js
@@ -0,0 +1,19 @@
+// 'Object literals' or 'singleton objects'
+let js1 = {
+    courseName: 'JavaScript intro',
+    weeks: 5,
+    madeBy: 'W3Cx',
+    author: 'Michel Buffa'
+};
+
+let darkVador1AsAnArray = ['villain', 'half human, half machine'];
+
+let darkVadorAsAnObject = {
+    job: 'villain',
+    race: 'half human, half machine'
+};
+
+let book = {
+    title: 'Le petit prince',
+    author: 'Saint-Exupery'
+};
